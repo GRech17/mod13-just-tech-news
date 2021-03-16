faad9b220d5b63fc695210a6ae24d9b20c102f15
GIR <gireches@gmail.com>
2021-03-15 22:12:44 -0700

    user authentication works

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const session = require('express-session');
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
             'id',
@@ -37,5 +39,14 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
     });
+
+router.get('/login', (req, res) => {
+    if (req/session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
+});    
 
 module.exports = router;
